e617aee6bab7d3d0ce00275a2f53b0807cc6e52c
luccacoelhos-jpg <luccacoelhos@gmail.com>
2026-05-04 14:15:33 +0000

    v2

--- seletor.py
+++ seletor.py
@@ -1,8 +1,10 @@
 #-- Seletor de caracteres --
-letra = input("Digite a letra")
-if letra == "a":
+print("Sistema seletor de caracteres")
+print("Escolha uma letra e veja se esta disponivel")
+letra = input("Digite a letra").upper()
+if letra =="a":
   print("Você escolheu a letra a")
-elif letra == "b":
+elif letra =="b":
   print("Você escolheu a letra b")
 else:
   print("Essa opção nao esta disponivel.")
