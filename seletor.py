5c26e64371f2aec480f4468912324b1a2c529bc3
luccacoelhos-jpg <luccacoelhos@gmail.com>
2026-05-04 09:33:40 -0300

    V1

---
+++ seletor.py
@@ -0,0 +1,8 @@
+#-- Seletor de caracteres --
+letra = input("Digite a letra")
+if letra == "a":
+  print("Você escolheu a letra a")
+elif letra == "b":
+  print("Você escolheu a letra b")
+else:
+  print("Essa opção nao esta disponivel.")
