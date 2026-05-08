e5a0bc1ae61767e8580b865ea404631518906912
luccacoelhos-jpg <luccacoelhos@gmail.com>
2026-05-08 14:28:08 +0000

    v2

---
+++ quizz.py
@@ -0,0 +1,22 @@
+#-- Quiz de Esportes⚽ --
+print("Quiz de Esportes!")
+print("1- Quem é o maior artilheiro da história da Champions League? A- Cristiano Ronaldo B- Messi C- Neymar Jr D- Pelé")
+resposta = input("Digite a resposta:").upper()
+if resposta =="A":
+  print("Resposta correta!")
+else:
+  print("Resposta errada!")
+
+print("2- Qual é a única seleção a ter participado de todas as edições da copa do mundo? A- Argentina B- Espanha C- Brasil D- México")
+resposta2 = input("Digite a resposta:").upper()
+if resposta2 =="C":
+  print("Resposta correta!")
+else:
+  print("Resposta errada!")
+
+print("3- Qual goleiro é o único na história a ter vencido a Bola de Ouro? A- Manuel Neuer B- Courtois C- Peter Cech D- Lev Yashin")
+resposta3 = input("Digite a resposta:").upper()
+if resposta3=="D":
+  print("Resposta correta!")
+else:
+  print("Resposta errada!")
