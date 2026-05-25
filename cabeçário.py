56ac4ceb57f3853cbae0701867a99ef26360900d
luccacoelhos-jpg <luccacoelhos@gmail.com>
2026-05-25 12:19:22 +0000

    v1

---
+++ cabeçário.py
@@ -0,0 +1,10 @@
+# --- Definido e Chamado a Função---
+def exibir_cabecalho():
+    print('=' * 30)
+    print('Sistema de gestão escolar')
+    print('versão 1.0 = desenvolvimento no codespace')
+    print('=' * 30)
+
+    # --- Chamando a funcão ---
+exibir_cabecalho()
+print('menu principal:')
